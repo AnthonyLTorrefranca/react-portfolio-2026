4dd459da7e6defcf01b67aac0c185820589545f7
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-03 00:42:17 +0800

    feat: added a component inside the navigation bar

--- react-portfolio-2026/src/components/NavBar.jsx
+++ react-portfolio-2026/src/components/NavBar.jsx
@@ -1,15 +1,19 @@
+import { Link } from 'react-router-dom'
+import sg from '../assets/SG.png'
 
-function NavBar() {
+export default function NavBar() {
   return (
-    <>
-      
-    </>
+    <nav className="flex justify-around items-center h-14 bg-black">
+    <div>
+      <img src={sg} className="h-15 top-0 cursor-pointer" />
+    </div>
+    <div>
+      <Link to="/" className="text-white p-4">HOME</Link>
+      <Link to="/bio" className="text-white p-4">BIO</Link>
+      <Link to="/projects" className="text-white p-4">PROJECTS</Link>
+      <Link to="/skills" className="text-white p-4">SKILLS</Link>
+      <Link to="/contact" className="text-white p-4">CONTACTS</Link>
+    </div>
+    </nav>
   )
 }
-
-NavBar.propTypes = {
-
-}
-
-export default NavBar
-
