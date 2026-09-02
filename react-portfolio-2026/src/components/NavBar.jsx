a9411c583ccb36983af4c9b7fbd18ee6bf70d7ff
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-03 00:48:51 +0800

    feat: added a link in the logo

--- react-portfolio-2026/src/components/NavBar.jsx
+++ react-portfolio-2026/src/components/NavBar.jsx
@@ -5,14 +5,16 @@ export default function NavBar() {
   return (
     <nav className="flex justify-around items-center h-14 bg-black">
     <div>
+      <a href="https://www.facebook.com/starkgraphics" target="_blank">
       <img src={sg} className="h-15 top-0 cursor-pointer" />
+      </a>
     </div>
     <div>
-      <Link to="/" className="text-white p-4">HOME</Link>
-      <Link to="/bio" className="text-white p-4">BIO</Link>
-      <Link to="/projects" className="text-white p-4">PROJECTS</Link>
-      <Link to="/skills" className="text-white p-4">SKILLS</Link>
-      <Link to="/contact" className="text-white p-4">CONTACTS</Link>
+      <Link to="/" className="text-white p-4 hover:underline">HOME</Link>
+      <Link to="/bio" className="text-white p-4 hover:underline">BIO</Link>
+      <Link to="/projects" className="text-white p-4 hover:underline">PROJECTS</Link>
+      <Link to="/skills" className="text-white p-4 hover:underline">SKILLS</Link>
+      <Link to="/contact" className="text-white p-4 hover:underline">CONTACTS</Link>
     </div>
     </nav>
   )
