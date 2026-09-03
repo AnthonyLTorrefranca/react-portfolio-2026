7a1cc16c4a5b03877d39234f0432e1356f0daaef
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-03 13:18:27 +0800

    refactor: the navigation bar and hero section

--- react-portfolio-2026/src/components/Home.jsx
+++ react-portfolio-2026/src/components/Home.jsx
@@ -2,11 +2,11 @@ import heroSection from '../assets/heroSection.png'
 
 export default function Home(){
   return(
-    <div className="relative -top-2">
+    <div className="relative -top-15 -z-10">
       <div className="border-2">
         <img src={heroSection}/>
       </div>
-      <div className="absolute top-5 right-1/3">
+      <div className="absolute top-50 right-1/3">
         <p className="ml-25 text-2xl text-white">INVEST IN MY CODE</p>
         <p className="mr-20 text-5xl text-white">Anthony Torrefranca</p>
       </div>
--- react-portfolio-2026/src/components/NavBar.jsx
+++ react-portfolio-2026/src/components/NavBar.jsx
@@ -4,31 +4,30 @@ export default function NavBar() {
   return (
     <nav className="flex justify-around items-center h-14">
     <a href="https://www.facebook.com/6saitama9" target="_blank" className="flex justify-around items-center gap-2">
-      <p className="text-white text-xl">ANTHONY</p>
-      <p className="text-white text-xl">TORREFRANCA</p>
+      <p className="text-white text-xl">ANTHONY TORREFRANCA</p>
       <span className="h-8 w-1 bg-white" ></span>
       <p className="text-yellow-300 text-xl">PORTFOLIO</p>
     </a>
     <div>
       <Link to="/" 
-      className="text-white p 4 
-        hover:underline
+        className="text-white
+        text-2xl p-4 
         hover:text-gray-400">HOME</Link>
       <Link to="/bio" 
-        className="text-white p 4 
-        hover:underline
+        className="text-white
+        text-2xl p-4 
         hover:text-gray-400">BIO</Link>
       <Link to="/projects"
-        className="text-white p-4 
-        hover:underline
+        className="text-white
+        text-2xl p-4 
         hover:text-gray-400">PROJECTS</Link>
       <Link to="/skills" 
-        className="text-white p 4 
-        hover:underline
+        className="text-white
+        text-2xl p-4 
         hover:text-gray-400">SKILLS</Link>
       <Link to="/contacts" 
-        className="text-white p-4 
-        hover:underline
+        className="text-white
+        text-2xl p-4 
         hover:text-gray-400">CONTACTS</Link>
     </div>
     </nav>
