2443cd573d3a8d858fcadc03c298e9153bc4d9c0
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-03 21:07:50 +0800

    feat: fixed the z index for navigation bar

--- react-portfolio-2026/src/components/Home.jsx
+++ react-portfolio-2026/src/components/Home.jsx
@@ -2,13 +2,20 @@ import heroSection from '../assets/heroSection.png'
 
 export default function Home(){
   return(
-    <div className="relative -top-15 -z-10">
+    <div className="relative -top-15">
       <div className="border-2">
-        <img src={heroSection}/>
+        <img src={heroSection} className="w-full h-auto object-cover"/>
       </div>
-      <div className="absolute top-50 right-1/3">
-        <p className="ml-25 text-2xl text-white">INVEST IN MY CODE</p>
-        <p className="mr-20 text-5xl text-white">Anthony Torrefranca</p>
+      <div className="absolute top-50 right-1/4">
+        <div className="flex flex-col justify-center ml-10 p-5">
+          <p className="ml-25 text-3xl text-white">INVEST IN MY CODE</p>
+        </div>
+        <div className="flex flex-col items-center py-10">
+          <p className="mr-20 text-5xl text-amber-300">ANTHONY TORREFRANCA,</p>
+          <p className="mr-20 text-5xl text-white">REACT JS DEVELOPER</p>
+        </div>
+        <p className="text-white w-200 py-10">I transform requirements into seamless, performant web applications. With a passion for clean code and user-centric design, I build responsive, scalable frontend solutions that aren't just for the season, they're build to evolve. Discover my approach to engineering applications that help your digital presence feel more robust, move faster, and drive more value every single day.</p>
+        <button className="bg-yellow-400 p-4 hover:cursor-pointer hover:bg-yellow-500"><span className="text-white">VIEW MY PROJECTS!</span></button>
       </div>
     </div>
   )
--- react-portfolio-2026/src/components/NavBar.jsx
+++ react-portfolio-2026/src/components/NavBar.jsx
@@ -2,34 +2,36 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-around items-center h-14">
-    <a href="https://www.facebook.com/6saitama9" target="_blank" className="flex justify-around items-center gap-2">
-      <p className="text-white text-xl">ANTHONY TORREFRANCA</p>
-      <span className="h-8 w-1 bg-white" ></span>
-      <p className="text-yellow-300 text-xl">PORTFOLIO</p>
-    </a>
-    <div>
-      <Link to="/" 
-        className="text-white
-        text-2xl p-4 
-        hover:text-gray-400">HOME</Link>
-      <Link to="/bio" 
-        className="text-white
-        text-2xl p-4 
-        hover:text-gray-400">BIO</Link>
-      <Link to="/projects"
-        className="text-white
-        text-2xl p-4 
-        hover:text-gray-400">PROJECTS</Link>
-      <Link to="/skills" 
-        className="text-white
-        text-2xl p-4 
-        hover:text-gray-400">SKILLS</Link>
-      <Link to="/contacts" 
-        className="text-white
-        text-2xl p-4 
-        hover:text-gray-400">CONTACTS</Link>
-    </div>
-    </nav>
+    <header className="fixed w-full top-0 left-0 flex justify-around items-center z-50">
+      <nav className="flex items-center gap-55">
+        <a href="https://www.facebook.com/6saitama9" target="_blank" className="flex justify-around items-center gap-2">
+          <p className="text-white text-xl">ANTHONY TORREFRANCA</p>
+          <span className="h-8 w-1 bg-white" ></span>
+          <p className="text-yellow-300 text-xl">PORTFOLIO</p>
+        </a>
+        <div className="flex">
+          <Link to="/" 
+            className="text-white
+            text-2xl p-4 
+            hover:text-gray-400">HOME</Link>
+          <Link to="/bio" 
+            className="text-white
+            text-2xl p-4 
+            hover:text-gray-400">BIO</Link>
+          <Link to="/projects"
+            className="text-white
+            text-2xl p-4 
+            hover:text-gray-400">PROJECTS</Link>
+          <Link to="/skills" 
+            className="text-white
+            text-2xl p-4 
+            hover:text-gray-400">SKILLS</Link>
+          <Link to="/contacts" 
+            className="text-white
+            text-2xl p-4 
+            hover:text-gray-400">CONTACTS</Link>
+        </div>
+      </nav>
+    </header>
   )
 }
