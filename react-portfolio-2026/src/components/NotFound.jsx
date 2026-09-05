e3afa307859db8a686575e7f16aa99ca171f3125
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-06 00:12:51 +0800

    refactor: NotFound component

--- react-portfolio-2026/src/components/NotFound.jsx
+++ react-portfolio-2026/src/components/NotFound.jsx
@@ -2,10 +2,9 @@ import { Link } from 'react-router-dom'
 
 export default function NotFound(){
     return(
-    <div className="w-screen h-screen bg-red-950">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <Link to="/">Go back home</Link>
+    <div className="flex items-center justify-center flex-col w-screen h-screen bg-red-950">
+      <p className="text-9xl font-bold">404 - PAGE NOT FOUND!</p>
+      <Link to="/"><button className="text-white p-4 border-2 border-white rounded-2xl active:bg-white my-10">Go back home</button></Link>
     </div>
     )
 }
