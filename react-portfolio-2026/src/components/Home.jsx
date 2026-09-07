9f81be78c783134531efce9b793100cb34abf72a
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-09-07 22:27:32 +0800

    refactor: home component linked to project section

--- react-portfolio-2026/src/components/Home.jsx
+++ react-portfolio-2026/src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import heroSection from '../assets/heroSection.png'
 
 export default function Home(){
@@ -12,7 +13,7 @@ export default function Home(){
         <p className="text-5xl text-amber-300">ANTHONY TORREFRANCA,</p>
         <p className="text-5xl text-white mb-8">REACT JS DEVELOPER</p>
         <p className="text-white max-w-2xl mb-10">I transform requirements into seamless, performant web applications. With a passion for clean code and user-centric design, I build responsive, scalable frontend solutions that aren't just for the season, they're build to evolve. Discover my approach to engineering applications that help your digital presence feel more robust, move faster, and drive more value every single day.</p>
-        <button className="bg-yellow-400 p-4 hover:cursor-pointer active:bg-yellow-500"><span className="text-white">VIEW MY PROJECTS!</span></button>
+        <Link className="bg-yellow-400 p-4 hover:cursor-pointer active:bg-yellow-500" to="/projects"><span className="text-white">VIEW MY PROJECTS!</span></Link>
       </div>
     </div>
   </div>
